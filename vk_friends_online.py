932b85fcc45fe50670248aa5f8dba2896f9f75dd
Dimasik <prevetek@gmail.com>
2016-10-15 16:30:28 +0300

    v 1.0
    Реализована авторизация, запрос пользователей и отбор тех, кто имеет
    статус ОНЛАЙН.

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,17 @@
 import vk
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5661578  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    login = input("Логин: ")
+    return login
 
 
 def get_user_password():
-    pass
+    password = input("Пароль: ")
+    return password
 
 
 def get_online_friends(login, password):
@@ -19,14 +21,27 @@ def get_online_friends(login, password):
         user_password=password,
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends = api.friends.get(fields=["online"])
+    friends_online = []
+    for friend in friends:
+        if friend["online"] == 1:
+            friends_online.append(friend)
+    return friends_online
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print("Сейчас онлайн {0:d} человек:".format(len(friends_online)))
+    for friend in friends_online:
+        print("  {0:s} {1:s}".format(friend["first_name"], friend["last_name"]))
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    try:
+        friends_online = get_online_friends(login, password)
+        output_friends_to_console(friends_online)
+    except vk.exceptions.VkAuthError:
+        pass
+    except:
+        print("ОШИБКА! Проверьте подключение к интернету и попробуйте еще раз.")
